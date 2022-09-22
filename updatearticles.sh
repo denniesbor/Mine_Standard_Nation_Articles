8e984a4d220bcbed3446a94413b71b3e5c433f53
Dennies Bor <borkiprono@gmail.com>
2022-09-23 00:10:30 +0300

    File Upload

---
+++ updatearticles.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo "starting python run commands"
+
+/home/denniesbor/Desktop/Mine_Standard_Nation_Articles/newsscraper/bin/python3.10 /home/denniesbor/Desktop/Mine_Standard_Nation_Articles/exportarticles.py
+
+echo "finished executing program"
